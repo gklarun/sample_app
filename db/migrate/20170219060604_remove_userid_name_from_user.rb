b751f1d7b4992d96975c29aca5faefd60aaeda5e
DRL <gklarun@gmail.com>
2017-02-20 20:04:56 +1300

    Production DB changes

--- db/migrate/20170219060604_remove_userid_name_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUseridNameFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :user_id
-    remove_column :users, :common_user
-    remove_column :users, :moderator
-    remove_column :users, :analyst
-    remove_column :users, :admin
-  end
-end
